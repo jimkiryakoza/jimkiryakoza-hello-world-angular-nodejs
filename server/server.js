0f1a70e206d81cf3de21d09e533fef2082011657
jimkiryakoza <159175567+jimkiryakoza@users.noreply.github.com>
2024-02-20 21:21:53 +0000

    Update to latest version of node.js

--- server/server.js
+++ server/server.js
@@ -1,28 +1,29 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 const cors = require('cors');
 const pdfParse = require('pdf-parse');
 
-
+// Setup CORS options
 const corsOptions = {
-    origin: 'https://shiny-fortnight-jjr5jp7jjjvxhqvq4-4200.app.github.dev'
-  };
-  
-  app.use(cors(corsOptions));
-  
-app.use(bodyParser.json());
+    origin: true
+};
+
+app.use(cors(corsOptions));
+
+// Use Express's built-in JSON parser middleware
+app.use(express.json());
 
 app.post('/extract-pdf-text', async (req, res) => {
     console.log("Incoming request to /extract-pdf-text");
     try {
         const pdfUrl = req.body.url;
+        // Use dynamic import for 'node-fetch'
         const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
         const response = await fetch(pdfUrl);
-        const arrayBuffer = await response.arrayBuffer();
-        // Convert ArrayBuffer to Buffer
-        const data = Buffer.from(arrayBuffer);
+
+        // In modern versions of node-fetch, use response.buffer() to get a Buffer directly
+        const data = await response.buffer();
         
         pdfParse(data).then(function(data) {
             res.send({ text: data.text });
